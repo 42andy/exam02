9fa56c8150afd4e143cd2c1419b260f92cff65ef
42randy <andystoll@proton.me>
2024-07-17 14:31:56 +0200

    fin 3

--- 4/rev_wstr.c
+++ 4/rev_wstr.c
@@ -5,7 +5,6 @@ int	main(int argc, char *argv[])
 	int	i;
 	int	start;
 	int	end;
-	int	space;
 
 	i = 0;
 	if (argc == 2)
@@ -13,25 +12,21 @@ int	main(int argc, char *argv[])
 		while (argv[1][i])
 			i++;
 		i--;
-		space = 0;
 		while (i >= 0)
 		{
-			while (i >= 0 && argv[1][i] == 32)
-				i--;
 			if (i < 0)
-				break;
+				break ;
 			end = i;
 			while (i >= 0 && argv[1][i] != 32)
 				i--;
-			start = i + 1;;
-			if (space == 1)
-				write(1, " ", 1);
-			space = 1;
+			start = i + 1;
 			while (start <= end)
 			{
 				write(1, &argv[1][start], 1);
 				start++;
 			}
+			if (i > 0)
+				write(1, " ", 1);
 			i--;
 		}
 	}
